ea92712855d0120be083e7887b87a46138edf130
temari1013 <smile21512211@icloud.com>
2026-03-22 17:40:44 +0900

    20260322-2

---
+++ wasabi/target/x86_64-unknown-uefi/debug/deps/wasabi-14df113523dfb15c.d
@@ -0,0 +1,5 @@
+/home/ishmael/mydev/hikalium_OS1/wasabi/target/x86_64-unknown-uefi/debug/deps/wasabi-14df113523dfb15c.efi: src/main.rs
+
+/home/ishmael/mydev/hikalium_OS1/wasabi/target/x86_64-unknown-uefi/debug/deps/wasabi-14df113523dfb15c.d: src/main.rs
+
+src/main.rs:
---
+++ wasabi/target/x86_64-unknown-uefi/debug/wasabi.d
@@ -0,0 +1,1 @@
+/home/ishmael/mydev/hikalium_OS1/wasabi/target/x86_64-unknown-uefi/debug/wasabi.efi: /home/ishmael/mydev/hikalium_OS1/wasabi/src/main.rs
