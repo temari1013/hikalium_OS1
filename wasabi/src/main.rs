28d8068f93f146d39e2f23cd8fd947d6be326d18
temari1013 <smile21512211@icloud.com>
2026-03-31 23:03:46 +0900

    2026-03-31

--- wasabi/src/main.rs
+++ wasabi/src/main.rs
@@ -1,20 +1,46 @@
 #![no_std]
 #![no_main]
+#![feature(offset_of)]
 
-#[no_mangle]
+use core::mem::offset_of;
+use core::mem::size_of;
+use core::panic::PanicInfo;
+use core::ptr::null_mut;
+use core::slice;
 
-const EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID: EFiGuid{
+type EfiVoid = u8;
+type EfiHandle = u64;
+type Result<T> = core::result::Result<T, &'static str>;
+
+
+#[repr(C)]
+#[derive(Clone,Copy,PartialEq,Eq,Debug)]
+struct EfiGuid {
+    pub data0: u32,
+    pub data1: u16,
+    pub data2: u16,
+    pub data3: [u8; 8],
+}
+
+const EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID: EfiGuid = EfiGuid {
     data0: 0x9042a9de,
     data1: 0x23dc,
     data2: 0x4a38,
-    data3: [0x96,0xfb,0x7a,0xde,0x80,0x51,0x6a],
+    data3: [0x96,0xfb,0x7a,0xde,0xd0,0x80,0x51,0x6a],
+};
+
+#[derive(Debug,PartialEq,Eq,Copy,Clone)]
+#[must_use]
+#[repr(u64)]
+enum EfiStatus{
+    Success = 0
 }
 
 #[repr(C)]
 struct EfiBootServicesTable{
     _reserved0: [u64; 40],
     locate_protocol: extern "win64" fn(
-        protocol; *const EFiGuid,
+        protocol: *const EfiGuid,
         registration: *const EfiVoid,
         interface: *mut *mut EfiVoid,
     ) -> EfiStatus,
@@ -27,27 +53,7 @@ struct EfiSystemTable {
     _reserved0: [u64; 12],
     pub boot_services: &'static EfiBootServicesTable,
 }
-const _: () = assert!(offset_of!(EfiBootServicesTable,boot_services) == 96);
-
-#[repr(C)]
-#[derive(Clone,Copy,PartialEq,Eq,Debug)]
-struct EFiGuid{
-    pub data0: u32,
-    pub data1: u16,
-    pub data2: u16,
-    pub data3[u8,8],
-}
-
-#[repr(C)]
-#[derive(Debug)]
-struct EfiGraphicsoutputProtocolMode<'a>{
-    pub max_mode:u32,
-    pub mode : u32,
-    pub info: &7a EfiGraphicsOutPutProtocolPixelInfo,
-    pub size?of?info: u64,
-    pub frame_buffer_base: usize,
-    pub frame_buffer_size: usize,
-}
+const _: () = assert!(offset_of!(EfiSystemTable,boot_services) == 96);
 
 #[repr(C)]
 #[derive(Debug)]
@@ -55,27 +61,48 @@ struct EfiGraphicsOutputProtocolPixelInfo{
     version: u32,
     pub horizonal_resolution: u32,
     pub vertical_resolution: u32,
-    _padding0: [u32,5],
+    _padding0: [u32; 5],
     pub pixels_per_scan_line :u32,
 }
-const _: () = assert!(size_of::<EfiGraphicsOutputProtolpixelInfo>() == 36);
+const _: () = assert!(size_of::<EfiGraphicsOutputProtocolPixelInfo>() == 36);
 
-fn locate_graphic_protocol<'a?(
+#[repr(C)]
+#[derive(Debug)]
+struct EfiGraphicsOutputProtocolMode<'a>{
+    pub max_mode:u32,
+    pub mode : u32,
+    pub info: &'a EfiGraphicsOutputProtocolPixelInfo,
+    pub size_of_info: u64,
+    pub frame_buffer_base: usize,
+    pub frame_buffer_size: usize,
+}
+
+#[repr(C)]
+#[derive(Debug)]
+struct EfiGraphicsOutputProtocol<'a> {
+    reserved: [u64; 3],
+    pub mode: &'a EfiGraphicsOutputProtocolMode<'a>,
+}
+fn locate_graphic_protocol<'a>(
     efi_system_table: &EfiSystemTable,
 ) -> Result<& 'a EfiGraphicsOutputProtocol<'a>> {
-    let mut grahic_output_protocol = null_mut::<EfiGraphiOutputProtocol>();
+    let mut graphic_output_protocol = null_mut::<EfiGraphicsOutputProtocol>();
     let status = (efi_system_table.boot_services.locate_protocol)(
         &EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID,
-        null_mut::EfiVoid(),
+        null_mut::<EfiVoid>(),
         &mut graphic_output_protocol as *mut *mut  EfiGraphicsOutputProtocol as *mut *mut EfiVoid,
-    )
+    );
+    if status != EfiStatus::Success {
+        return Err("Failed to locate Graphics Output Protocol");
+    }
     Ok(unsafe { &*graphic_output_protocol })
 }
 
-fn efi_main(_image_handle: Efihandle, efi_system_table: &EfiSystemTable) {
+#[no_mangle]
+fn efi_main(_image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
     let efi_graphics_output_protocol = locate_graphic_protocol(efi_system_table).unwrap();
-    let vram_addr = efi_graphics_output_protocol.mode_frame_buffer_base;
-    let vram_byte_size =efi_graphics_output_protocol.mode_frame_buffer_base;
+    let vram_addr = efi_graphics_output_protocol.mode.frame_buffer_base;
+    let vram_byte_size =efi_graphics_output_protocol.mode.frame_buffer_size;
     let vram = unsafe {
         slice::from_raw_parts_mut(vram_addr as *mut u32,vram_byte_size / size_of::<u32>())
     };
@@ -84,8 +111,6 @@ fn efi_main(_image_handle: Efihandle, efi_system_table: &EfiSystemTable) {
     }
     loop {}
 }
-
-use core::panic::PanicInfo;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> !{
