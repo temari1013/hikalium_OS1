e5e437e63ebf0aaa93a9232e2b5f4df9953bccaf
temari1013 <smile21512211@icloud.com>
2026-04-10 00:33:03 +0900

    20260409

--- wasabi/src/main.rs
+++ wasabi/src/main.rs
@@ -39,6 +39,80 @@ enum EfiStatus {
     Success = 0,
 }
 
+#[repr(i64)]
+#[derive(Debug, Clone ,Copy, PartialEq,Eq)]
+#[allow(non_camel_case_types)]
+pub enum EfiMemoryType {
+    RESERVED = 0,
+    LOADER_CODE,
+    LOADER_DATA,
+    BOOT_SERVICES_CODE,
+    BOOT_SERVICES_DATA,
+    RUNTIME_SERVICES_CODE,
+    RUNTIME_SERVICES_DATA,
+    CONVENTIONAL_MEMORY,
+    UNUSABLE_MEMORY,
+    ACPI_RECLAIM_MEMORY,
+    ACPI_MEMORY_NVS,
+    MEMORY_MAPPED_IO,
+    MEMORY_MAPPED_IOPORT_SPACE,
+    PAL_CODE,
+    PERSISTENT_MEMORY,
+}
+
+#[repr(C)]
+#[derive(Clone,Copy, PartialEq, Eq, Debug)]
+struct EfiMemoryDescriptor {
+    memory_type:EfiMemoryType,
+    physical_start: u64,
+    virtual_start: u64,
+    number_of_pages: u64,
+    attribute: u64,
+}
+
+const MEMORY_MAP_BUFFER_SIZE: usize = 0x8000;
+
+struct MemoryMapHolder {
+    memory_map_buffer: [u8;MEMORY_MAP_BUFFER_SIZE],
+    memory_map_size: usize,
+    map_key: usize,
+
+    descriptor_size: usize,
+    descriptor_version:u32,
+}
+struct MemoryMapIterator<'a> {
+    map: &'a MemoryMapHolder,
+    ofs: usize,
+}
+impl<'a> Iterator for MemoryMapIterator<'a>{
+    type Item =  &'a EfiMemoryDescriptor;
+    fn next(&mut self) -> Option<&'a EfiMemoryDescriptor> {
+        if self.ofs >= self.map.memory_map_size {
+            None
+        } else {
+            let e: &EfiMemoryDescriptor = unsafe {
+                &*(self.map.memory_map_buffer.as_ptr().add(self.ofs) as *const EfiMemoryDescriptor)
+            };
+            self.ofs += self.map.descriptor_size;
+            Some(e)
+        }
+    }
+}
+impl MemoryMapHolder {
+    pub const fn new() -> MemoryMapHolder{
+        MemoryMapHolder {
+            memory_map_buffer: [0; MEMORY_MAP_BUFFER_SIZE],
+            memory_map_size: MEMORY_MAP_BUFFER_SIZE,
+            map_key:0,
+            descriptor_size: 0,
+            descriptor_version:0,
+        }
+    }
+    pub fn iter(&self) -> MemoryMapIterator {
+        MemoryMapIterator {map:self, ofs:0}
+    }
+}
+
 #[repr(C)]
 struct EfiBootServicesTable {
     _reserved0: [u64; 7],
@@ -49,7 +123,7 @@ struct EfiBootServicesTable {
         descriptor_size: *mut usize,
         descriptor_version: *mut u32,
     ) -> EfiStatus,
-    _reserved1: [u64,32],
+    _reserved1: [u64;32],
 
     locate_protocol: extern "win64" fn(
         protocol: *const EfiGuid,
@@ -58,16 +132,17 @@ struct EfiBootServicesTable {
     ) -> EfiStatus,
 }
 impl EfiBootServicesTable {
-    fn get_memory_map(&self, map: &mut MemorymapHolder) -> EfiStatus{
-        (self.get_memory_map){
+    fn get_memory_map(&self, map: &mut MemoryMapHolder) -> EfiStatus{
+        (self.get_memory_map)(
             &mut map.memory_map_size ,
             map.memory_map_buffer.as_mut_ptr(),
             &mut map.map_key,
             &mut map.descriptor_size,
             &mut map.descriptor_version,
-        }
+    )
     }
 }
+const _: () =  assert!(offset_of!(EfiBootServicesTable, get_memory_map) == 56);
 const _: () = assert!(offset_of!(EfiBootServicesTable, locate_protocol) == 320);
 
 #[repr(C)]
@@ -169,6 +244,22 @@ fn efi_main(_image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
         writeln!(w, "i={i}").unwrap();
     }
 
+    let mut memory_map = MemoryMapHolder::new();
+    let status = efi_system_table.boot_services.get_memory_map(&mut memory_map);
+    writeln!(w,"{status:?}").unwrap();
+    let mut total_memory_pages = 0;
+    for e in memory_map.iter() {
+        if e.memory_type != EfiMemoryType::CONVENTIONAL_MEMORY {
+            continue;
+        }
+        total_memory_pages += e.number_of_pages;
+        writeln!(w,"{e:?}").unwrap();
+    }
+    let total_memory_size_mib = total_memory_pages*4096 / 1024/1024;
+    writeln!(
+        w,
+        "Total: {total_memory_pages} pages = {total_memory_size_mib} MIB"
+    ).unwrap();
     loop {
         hlt()
     }
@@ -390,35 +481,4 @@ impl fmt::Write for VramTextWriter<'_>{
         }
         Ok(())
     }
-}
-
-#[repr(C)]
-#[derive(Clone,Copy, PartialEq, Eq, Debug)]
-struct EfiMemoryDescriptor {
-    memory_tipe: EfiMemoryType,
-    physical_start: u64,
-    virtual_start, u64,
-    number_of_pages: u64,
-    attribute: u64,
-}
-
-#[repr(i64)]
-#[derive(Debug, Clone ,Copy, PartialEq,Eq)]
-#[allow(non_camel_case_types)]
-pub enum EfimemoryType {
-    RESERVED = 0;
-    LOADER_CODE,
-    LOADER_DATA,
-    BOOT_SERVICES_CODE,
-    BOOT_SERVICES_DATA,
-    RUNTIME_SERVICES_CODE,
-    RUNTIME_SERVICES_DATA,
-    CONVENTIONAL_MEMORY,
-    UNUSABLE_MEMORY,
-    ACPI_RECLAIM_MEMORY,
-    ACPI_MEMORY_NVS,
-    MEMORY_MAPPED_IO,
-    MEMORY_MAPPED_IOPORT_SPACE,
-    PAL_CODE,
-    PERSISTENT_MEMORY,
 }
