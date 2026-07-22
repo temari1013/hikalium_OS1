d8ae3f3b6a0db33bcf60cec0a78aa0c87022c9ed
temari1013 <smile21512211@cloud.com>
2026-07-22 19:40:47 +0900

    途中

--- wasabi/src/acpi.rs
+++ wasabi/src/acpi.rs
@@ -1,5 +1,6 @@
 use crate::hpet::HpetRegisters;
 use crate::result::Result;
+use core::fmt::{self, write};
 use core::mem::size_of;
 
 #[repr(packed)]
@@ -138,5 +139,65 @@ impl AcpiRsdpStruct{
     pub fn hpet(&self) -> Option<&AcpiHpetDescriptor> {
         let xsdt = self.xsdt();
         xsdt.find_table(b"HPET").map(AcpiHpetDescriptor::new)
+    }
+    pub fn mcfg(&self) -> Option<&AcpiMcfgDescriptor> {
+        let xsdt = self.xsdt();
+        xsdt.find_table(b"MCFG").map(AcpiMcfgDescriptor::new)
+    }
+}
+
+#[repr(C)]
+#[derive(Debug)]
+#[allow(dead_code)]
+pub struct AcpiMcfgdescriptor{
+    header : SystemDescriptionTableHeader,
+    _unused: [u8;8],
+}
+impl AcpiTable for AcpiMcfgdescriptor {
+    const SIGNATURE:&'static[u8;4] = b"MCFG";
+    type Table = Self;
+}
+const _: () = assert!(size_of::<AcpiHpetDescriptor>() == 44);
+
+impl AcpiMcfgdescriptor{
+    pub fn header_size(&self) -> usize{
+        size_of::<Self>()
+    }
+    pub fn num_of_entries(&self) -> usize{ 
+        (self.header.length as usize - self.header_size()) / size_of<EcamEntry>()
+
+    }
+    pub fn entry(&self , index:usize) -> Option<&EcamEntry>{
+        if index >= self.num_of_entries() {
+            None
+        }else {
+            Some(unsafe {
+                &* ((self as *const Self as *const u8).add(self.header_size())as *const EcamEntry ). add(index)
+            })
+        }
+    }
+}
+
+#[repr(packed)]
+pub struct EcamEntry {
+    ecm_base_addr: u64,
+    _pci_segment_group: u64 ,
+    start_pci_bus: u8,
+    end_pci_bus : u8 ,
+    _reserved: u32,
+}
+impl EcamEntry{
+    pub fn base_address(&self) -> u64{
+        self.ecm_base_addr
+    }
+}
+impl fmt::Display for EcamEntry {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let base = self.ecm_base_addr;
+        let bus_start = self.start_pci_bus;
+        let bus_end = self.end_pci_bus;
+        write!8
+        f ,
+        "ECAM: Bus[{} ..= {}]"
     }
 }
